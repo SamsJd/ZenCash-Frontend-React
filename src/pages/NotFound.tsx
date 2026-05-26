5741b03998bc29f4b02f3a272c3666bf52dd82f3
Sâmara Jeise Dias <samarajeise@hotmail.com>
2026-05-26 01:47:57 -0300

    feat: implementando estrutura principal do frontend com CRUDs e integração REST

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
-      <p>Página não encontrada</p>
+    <div className="container text-center py-5 text-white">
+
+      <h1
+        style={{
+          fontSize: "120px",
+          fontWeight: "bold",
+        }}
+      >
+        404
+      </h1>
+
+      <h2 className="mb-3">
+        Página não encontrada
+      </h2>
+
+      <p className="mb-4">
+        A página que você tentou acessar não existe.
+      </p>
+
+      <Link to="/" className="btn btn-light">
+        Voltar para Home
+      </Link>
+
     </div>
   );
 }
